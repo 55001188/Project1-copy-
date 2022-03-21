7b0b21447125a3d411d8019ef5cfbd49e55c0070
olivia <olivia@slayer>
2022-03-20 22:55:34 -0700

    closer to finish

--- Project1(copy)/src/P1.java
+++ Project1(copy)/src/P1.java
@@ -14,27 +14,42 @@ public class P1 {
 		File g = new File("oliviaMap3.txt");
 		//
 		//oliviaMap3
+		String helpInfo = "This program is supposed to process maps and return the map with the path marked" 
+				+"\n"+ "SWITCHES:" +"\n"+ "Stack: uses the stack based approach to solve for the path"+"\n"+
+				"Queue: uses the queue based approach to solve for the path"+"\n"+"Opt: finds the shortest path"+
+				"\n"+"Time: prints the runtime of the program"+"\n"+
+				"Incoordinate: if set, the input file is in the coordinate-based system, else it is in the"
+				+ " text-map format"+"\n"+"Outcoordinate: if set, the output file is in the coordinate based system"
+				+", else it is in the text-map format"+"\n"+"Help: prints out what the program is meant to do"+
+				" and what the switches are and what they do";
+		//System.out.println(helpInfo);
 		
 		try {
 			scanner = new Scanner(f);
 			Solver p = new Solver(scanner, g);
-			p.getMap().getCharMap();
-			p.checkMapOp();
+			//p.getMap().getCharMap();
+			//p.checkMapOp();
 			//p.checkMapQueue();
 			//p.checkMapStack();
-			p.roomPath();
+			//p.roomPath();
 			//p.getPathCoordsMap();
 			
 			
 			/*
-			 * stack and path and direct path work
+			 * stack
+			 * queue
+			 * opt
+			 * time
+			 * incoordinate
+			 * outcoordinate
+			 * help
 			 */
 		} catch (FileNotFoundException e) {
 			
 		}
 		
 		long total = System.currentTimeMillis() - startTime;
-		System.out.print(total);
+		//System.out.print(total);
 	}
 
 }
